cc0c195891c8bc5748070e68ce7a92460b56b090
zenessexprimetoi <102719593+zenessexprimetoi@users.noreply.github.com>
2022-04-07 20:17:58 +0400

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,60 @@
+$(document).ready(function() {
+    $('#autoWidth1').lightSlider({
+        autoWidth:true,
+        loop:true,
+        onSliderLoad: function(){
+            $('#autoWidth1').removeClass('cS-hidden');
+        }
+
+    });
+
+});
+
+$(document).ready(function() {
+    $('#autoWidth2').lightSlider({
+        autoWidth:true,
+        loop:true,
+        onSliderLoad: function(){
+            $('#autoWidth2').removeClass('cS-hidden');
+        }
+
+    });
+
+});
+
+$(document).ready(function() {
+    $('#autoWidth3').lightSlider({
+        autoWidth:true,
+        loop:true,
+        onSliderLoad: function(){
+            $('#autoWidth3').removeClass('cS-hidden');
+        }
+
+    });
+
+});
+
+$(document).ready(function() {
+    $('#autoWidth4').lightSlider({
+        autoWidth:true,
+        loop:true,
+        onSliderLoad: function(){
+            $('#autoWidth4').removeClass('cS-hidden');
+        }
+
+    });
+
+});
+
+$(document).ready(function() {
+    $('#autoWidth5').lightSlider({
+        autoWidth:true,
+        loop:true,
+        onSliderLoad: function(){
+            $('#autoWidth5').removeClass('cS-hidden');
+        }
+
+    });
+
+});
+
